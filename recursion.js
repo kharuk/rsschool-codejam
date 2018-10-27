db823fa85bb1b76ef5dd6ea218237882e40eb00f
kharuk <frends.nastya@gmail.com>
2018-10-28 01:25:34 +0300

    feat: implement recursion function

---
+++ recursion.js
@@ -0,0 +1,20 @@
+function recursion(tree) {
+    let resultArray = [];
+
+    function treeTraversal(node, arr, index) {
+        if (!arr[index]){
+           arr[index] = [];
+        }
+        arr[index].push(node.value);
+
+        if (node.left){
+            treeTraversal(node.left, arr, index+1);
+        }
+        if (node.left){
+            treeTraversal(node.right, arr, index+1);
+        }
+    }
+
+    treeTraversal(tree, resultArray, 0);
+    return resultArray;
+}
