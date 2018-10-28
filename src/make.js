6c5ab0dea7ebaeb0ada79a8b07d5a2ed4c1c82c3
kharuk <frends.nastya@gmail.com>
2018-10-28 09:47:37 +0300

    feat: implement make.js function

---
+++ src/make.js
@@ -0,0 +1,9 @@
+function make (...arg){
+    return function f(...items){
+        if (typeof  items[0] === 'function'){
+            return arg.reduce(items[0]);
+        }
+        items.forEach(item => arg.push(item));
+        return f;
+    }
+}
