06e47ea87c6ef1a8f90b854c5932e84f83304d2c
kharuk <frends.nastya@gmail.com>
2018-10-28 17:50:00 +0300

    style: change for eslint

--- src/make.js
+++ src/make.js
@@ -1,9 +1,9 @@
-function make (...arg){
-    return function f(...items){
-        if (typeof  items[0] === 'function'){
-            return arg.reduce(items[0]);
-        }
-        items.forEach(item => arg.push(item));
-        return f;
+module.exports = function make(...arg) {
+  return function f(...items) {
+    if (typeof items[0] === 'function') {
+      return arg.reduce(items[0]);
     }
-}
+    items.forEach(item => arg.push(item));
+    return f;
+  };
+};
--- src/sumOfOther.js
+++ src/sumOfOther.js
@@ -1,4 +1,4 @@
-function sumOfOther (array){
-    let sumOfAllElement = array.reduce(((sum,item) => sum + item),0);
-    return array.map( (item) => sumOfAllElement - item);
-}
+module.exports = function sumOfOther(array) {
+  const sumOfAllElement = array.reduce(((sum, item) => sum + item), 0);
+  return array.map(item => sumOfAllElement - item);
+};
